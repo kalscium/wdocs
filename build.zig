d2dc674fcfb1b99f467b207cfc35abdd89f016e3
kalscium <kalscium@protonmail.com>
2025-06-05 09:26:00 +1000

    added blueprints, updated build.zig & cleaned src
    I've added some 'blueprints' that outline my plan for how the wdocs
    program should operate.
    
    I've also updated build.zig so that the exe can call lib functions.
    
    And also cleared the pre-existing template code in main.zig & root.zig.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

--- build.zig
+++ build.zig
@@ -36,6 +36,8 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
+    exe.root_module.addImport("wdocs", lib.root_module);
+    
     // This declares intent for the executable to be installed into the
     // standard location when the user invokes the "install" step (the default
     // step when running `zig build`).
