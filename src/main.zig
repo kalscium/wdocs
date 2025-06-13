3be74b9c2fb62c5ed190b2773aa9610312020fe5
kalscium <kalscium@protonmail.com>
2025-06-13 22:03:40 +1000

    added the collectWord & collectNumber functions
    I've implemented simple collect word and collect number functions that
    simply read from a slice, incrementing a sort of cursor into the
    slice collecting as many valid values as possible before returning.
    
    I could call this 'greedy parsing'.
    
    Also, for collecting numbers, I've written my own algorithm to convert
    them into their numerical values. It's probably far for the most
    optimal solution, but it's cool that I did it myself without any help
    or external resources.
    
    I've also removed gen.zig from the blueprint, as following the zig style
    guide, general functions for the entire project should be placed within
    root.zig.
    
    Signed-off-by: Ethan Ma <kalscium@protonmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,4 +1,19 @@
 const std = @import("std");
+const wdocs = @import("wdocs");
 
 pub fn main() !void {
+    const slice = "hello-world, or something else__ + 01234";
+    var idx: usize = 0;
+
+    while (idx != slice.len) {
+        const word = wdocs.collectWord(slice, &idx);
+
+        // if no match, skip character
+        if (word.len == 0) {
+            idx += 1;
+            continue;
+        }
+        
+        std.debug.print("word: {s}\n", .{word});
+    }
 }
--- src/root.zig
+++ src/root.zig
@@ -1,1 +1,60 @@
+// pub const metadata = @import("metadata.zig");
 
+const std = @import("std");
+
+// // for tests
+// comptime {
+//     _ = metadata;
+// }
+
+/// Collects the next word into a slice (a-zA-Z_-), assuming the cursor
+/// is on the first character of the word
+pub fn collectWord(slice: []const u8, idx: *usize) []const u8 {
+    const start_idx = idx.*;
+    while (idx.* < slice.len) : (idx.* += 1) {
+        const char = slice[idx.*];
+        if ((char < 'A' or char > 'z') and char != '_' and char != '-' and (char < '0' or char > '9'))
+            break;
+    }
+    return slice[start_idx..idx.*];
+}
+
+/// Collects the next word into a slice (a-zA-Z_-), assuming the cursor
+/// is on the first character of the word
+pub fn collectNumber(slice: []const u8, idx: *usize) usize {
+    const start_idx = idx.*;
+    while (idx.* < slice.len) : (idx.* += 1) {
+        const char = slice[idx.*];
+        if (char < '0' or char > '9')
+            break;
+    }
+    const raw_num = slice[start_idx..idx.*];
+
+    // convert string to a number
+    var number: usize = 0;
+    for (raw_num, 0..) |char, i|
+        // the number characters are sequential, so if you subtract the
+        // character zero's value, you will find the numerical value of
+        // that number.
+        // 
+        // then the value is also multiplied with it's exponent
+        // (like scientific notation) with the largest order of
+        // magnitude is the first character
+        number += @as(usize, char - '0') * std.math.pow(usize, 10, raw_num.len-i-1);
+
+    return number;
+}
+
+test collectNumber {
+    const slice = "1234hello-world~ &separator";
+    var idx: usize = 0;
+    const number = collectNumber(slice, &idx);
+    std.debug.assert(number == 1234);
+}
+
+test collectWord {
+    const slice = "hello-world1234~ &separator";
+    var idx: usize = 0;
+    const collected = collectWord(slice, &idx);
+    std.debug.assert(std.mem.eql(u8, collected, "hello-world1234"));
+}
